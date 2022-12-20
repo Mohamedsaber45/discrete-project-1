c5dd739ab5b5c48f49cffde809c2bed9ee854f0c
Mohamedsaber45 <120626930+Mohamedsaber45@users.noreply.github.com>
2022-12-20 20:04:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter v");
+            double v =double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number");
+            for(double number = double.Parse(Console.ReadLine()); number <= v;number++)
+            {
+                double sum = 0;
+                for(double i=1;i<number;i++)
+                {
+                    if(number % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if(sum == number)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
